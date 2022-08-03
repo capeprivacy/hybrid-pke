b06b23d9d18473f7d5f06c745c52d51de53c59c9
jvmncs <jvmncs@gmail.com>
2022-08-03 16:02:18 -0400

    add bindings for hpke configuration + related python tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,34 @@
 use hacspec_lib::{ByteSeq, Seq, U8};
 use hpke::{
     AdditionalData, Ciphertext, HPKECiphertext, HPKEConfig, HpkeOpen, HpkePrivateKey,
-    HpkePublicKey, HpkeSeal, KemOutput, Mode,
+    HpkePublicKey, HpkeSeal, KemOutput,
 };
-use hpke_aead::AEAD;
 use hpke_errors::HpkeError;
-use hpke_kdf::{Info, InputKeyMaterial, KDF};
-use hpke_kem::{DeriveKeyPair, Nsk, Randomness, KEM};
+use hpke_kdf::Info;
+use hpke_kem::{GenerateKeyPair, Nenc, Nsk, Randomness, SerializePublicKey, KEM};
 use pyo3::exceptions::PyRuntimeError;
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 use rand::{rngs::OsRng, RngCore};
 
-fn get_default_hpke_config() -> HPKEConfig {
-    let mode = Mode::mode_base;
-    let kem = KEM::DHKEM_X25519_HKDF_SHA256;
-    let kdf = KDF::HKDF_SHA256;
-    let aead = AEAD::ChaCha20Poly1305;
-    HPKEConfig(mode, kem, kdf, aead)
-}
+pub mod config;
+use crate::config::*;
 
-fn hpke_open_bytes(ctxtb: &[u8], skb: &[u8]) -> Result<Vec<u8>, HpkeError> {
-    let hpke_config = get_default_hpke_config();
-    let kem_output = KemOutput::from_public_slice(&ctxtb[..32]);
-    let ctxt = Ciphertext::from_public_slice(&ctxtb[32..]);
+fn hpke_open_bytes(config: HPKEConfig, ctxtb: &[u8], skb: &[u8]) -> Result<Vec<u8>, HpkeError> {
+    let kem = config.1;
+    let n_enc = Nenc(kem);
+    let kem_output = KemOutput::from_public_slice(&ctxtb[..n_enc]);
+    let ctxt = Ciphertext::from_public_slice(&ctxtb[n_enc..]);
     let ciphertext = HPKECiphertext(kem_output, ctxt);
     let sk_r = HpkePrivateKey::from_public_slice(skb);
     let info = Info::new(0);
     let aad = AdditionalData::new(0);
-    let result: ByteSeq = HpkeOpen(
-        hpke_config,
-        &ciphertext,
-        &sk_r,
-        &info,
-        &aad,
-        None,
-        None,
-        None,
-    )?;
+    let result: ByteSeq = HpkeOpen(config, &ciphertext, &sk_r, &info, &aad, None, None, None)?;
     let plaintext = result.into_native();
     Ok(plaintext)
 }
 
-fn hpke_seal_bytes(pkb: &[u8], ptxtb: &[u8]) -> Result<Vec<u8>, HpkeError> {
-    let hpke_config = get_default_hpke_config();
+fn hpke_seal_bytes(config: HPKEConfig, pkb: &[u8], ptxtb: &[u8]) -> Result<Vec<u8>, HpkeError> {
     let pk = HpkePublicKey::from_public_slice(pkb);
     let ptxt = Seq::<U8>::from_public_slice(ptxtb);
     let info = Info::new(0);
@@ -52,15 +37,7 @@ fn hpke_seal_bytes(pkb: &[u8], ptxtb: &[u8]) -> Result<Vec<u8>, HpkeError> {
     OsRng.fill_bytes(&mut rand_bytes);
     let randomness = Randomness::from_public_slice(&rand_bytes);
     let result: HPKECiphertext = HpkeSeal(
-        hpke_config,
-        &pk,
-        &info,
-        &aad,
-        &ptxt,
-        None,
-        None,
-        None,
-        randomness,
+        config, &pk, &info, &aad, &ptxt, None, None, None, randomness,
     )?;
     let mut encapsulated: Vec<u8> = result.0.into_native();
     let mut ciphertext = result.1.into_native();
@@ -68,51 +45,68 @@ fn hpke_seal_bytes(pkb: &[u8], ptxtb: &[u8]) -> Result<Vec<u8>, HpkeError> {
     Ok(encapsulated)
 }
 
-fn generate_keypair() -> Result<(Vec<u8>, Vec<u8>), HpkeError> {
-    let hpke_config = get_default_hpke_config();
-    let kem = hpke_config.1;
+fn generate_keypair(kem: KEM) -> Result<(Vec<u8>, Vec<u8>), HpkeError> {
     let nbytes = Nsk(kem);
     let mut rand_bytes = vec![0u8; nbytes];
     OsRng.fill_bytes(&mut rand_bytes);
-    let randomness = InputKeyMaterial::from_public_slice(&rand_bytes);
-    let keypair = DeriveKeyPair(kem, &randomness)?;
-    Ok((keypair.0.into_native(), keypair.1.into_native()))
+    let randomness = Randomness::from_public_slice(&rand_bytes);
+    let keypair = GenerateKeyPair(kem, randomness)?;
+    let priv_key = keypair.0;
+    let pub_key = SerializePublicKey(kem, &keypair.1);
+    Ok((priv_key.into_native(), pub_key.into_native()))
 }
 
 #[pyfunction]
-fn generate_hpke_keypair(py: Python) -> PyResult<(&PyBytes, &PyBytes)> {
-    let keypair_bytes = generate_keypair()
-        .map_err(|hpke_error| PyRuntimeError::new_err(format!("{hpke_error:?}")))?;
+#[pyo3(name = "generate_keypair")]
+fn generate_keypair_py(py: Python, kem: PyKEM) -> PyResult<(&PyBytes, &PyBytes)> {
+    let keypair_bytes = generate_keypair(kem.into())
+        .map_err(|hpke_error| PyRuntimeError::new_err(format!("{hpke_error:#?}")))?;
     let sk_py = PyBytes::new(py, &keypair_bytes.0);
     let pk_py = PyBytes::new(py, &keypair_bytes.1);
     Ok((sk_py, pk_py))
 }
 
-/// Python binding to hpke-spec's Single-Shot API function hpke::HpkeSeal
+/// PyO3 binding to hpke-spec's Single-Shot API function hpke::HpkeSeal
 #[pyfunction]
-fn hpke_seal<'p>(py: Python<'p>, pk_py: &PyBytes, ptxt_py: &PyBytes) -> PyResult<&'p PyBytes> {
+fn seal<'p>(
+    py: Python<'p>,
+    pk_py: &PyBytes,
+    ptxt_py: &PyBytes,
+    config_py: PyHPKEConfig,
+) -> PyResult<&'p PyBytes> {
     let pkb = pk_py.as_bytes();
     let ptxtb = ptxt_py.as_bytes();
-    let ciphertext_bytes = hpke_seal_bytes(pkb, ptxtb)
-        .map_err(|hpke_error| PyRuntimeError::new_err(format!("{hpke_error:?}")))?;
+    let ciphertext_bytes = hpke_seal_bytes(config_py.into(), pkb, ptxtb)
+        .map_err(|hpke_error| PyRuntimeError::new_err(format!("{hpke_error:#?}")))?;
     Ok(PyBytes::new(py, &ciphertext_bytes))
 }
 
-/// Python binding to hpke-spec's Single-Shot API function hpke::HpkeOpen
+/// PyO3 binding to hpke-spec's Single-Shot API function hpke::HpkeOpen
 #[pyfunction]
-fn hpke_open<'p>(py: Python<'p>, sk_py: &PyBytes, ctxt_py: &PyBytes) -> PyResult<&'p PyBytes> {
+fn open<'p>(
+    py: Python<'p>,
+    sk_py: &PyBytes,
+    ctxt_py: &PyBytes,
+    config_py: PyHPKEConfig,
+) -> PyResult<&'p PyBytes> {
     let skb = sk_py.as_bytes();
     let ctxtb = ctxt_py.as_bytes();
-    let plaintext_bytes = hpke_open_bytes(ctxtb, skb)
-        .map_err(|hpke_error| PyRuntimeError::new_err(format!("{hpke_error:?}")))?;
+    let plaintext_bytes = hpke_open_bytes(config_py.into(), ctxtb, skb)
+        .map_err(|hpke_error| PyRuntimeError::new_err(format!("{hpke_error:#?}")))?;
     Ok(PyBytes::new(py, &plaintext_bytes))
 }
 
-/// A Python module implemented in Rust.
+/// PyO3 module for hpke-spec.
 #[pymodule]
 fn hpke_spec(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(generate_hpke_keypair, m)?)?;
-    m.add_function(wrap_pyfunction!(hpke_open, m)?)?;
-    m.add_function(wrap_pyfunction!(hpke_seal, m)?)?;
+    m.add_class::<PyHPKEConfig>()?;
+    m.add_class::<PyAEAD>()?;
+    m.add_class::<PyKDF>()?;
+    m.add_class::<PyKEM>()?;
+    m.add_class::<PyMode>()?;
+    m.add_function(wrap_pyfunction!(default_config, m)?)?;
+    m.add_function(wrap_pyfunction!(generate_keypair_py, m)?)?;
+    m.add_function(wrap_pyfunction!(open, m)?)?;
+    m.add_function(wrap_pyfunction!(seal, m)?)?;
     Ok(())
 }
